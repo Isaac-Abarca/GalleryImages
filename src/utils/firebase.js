7897bee3e98722ffca9e1144e456a37521909555
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-01 20:32:18 -0600

    Changed config .env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_X1YZ2W,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_V3WB8T,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_P7HK9A,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_S5JD3F,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_U4QM2N,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID_Z9XJ7K,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_W0LR5G,
 };
 
 // Inicializar Firebase
